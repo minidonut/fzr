f475e94f766cb2837960c706e9c26fd64f25e7bc
minidonut <nycom13@gmail.com>
2020-08-30 22:18:37 +0900

    add index function to be router

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,34 @@
-console.log("hello fzr");
+import { env } from './env';
+import * as command from './command';
+
+(async (): Promise<void> => {
+  switch (env.cmd) {
+    case 'version':
+      break;
+    case 'init':
+      break;
+    case 'status':
+      break;
+    case 'help':
+      break;
+    case 'config':
+      break;
+    case 'add':
+      break;
+    case 'rm':
+      break;
+    case 'update':
+      break;
+    case 'refresh':
+      break;
+    case 'export':
+      break;
+    case 'import':
+      break;
+    default:
+      if (env.cmd != null) {
+        command.notSupported(env.cmd);
+      }
+      break;
+  }
+})();
