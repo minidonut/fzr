efcce4a6f7aa8e85d4915ca24434f651bc5bb8f3
minidonut <nycom13@gmail.com>
2020-08-31 01:56:15 +0900

    update error handling

--- src/error/handleException.ts
+++ src/error/handleException.ts
@@ -1,11 +1,10 @@
-import { UnSupportedCommandError, NotImplementedCommandError } from './Errors';
+import { BaseError } from './Errors';
 export async function handleException(e: Error): Promise<void> {
-  if (e instanceof UnSupportedCommandError || e instanceof NotImplementedCommandError) {
+  if (e instanceof BaseError) {
     console.error(e.message);
+    process.exit(1);
   } else {
     // unhandled;
     throw e;
   }
-
-  process.exit(1);
 }
