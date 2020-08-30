a1a348b8d6ef41c11985f718135b6aee95c2ffdc
minidonut <nycom13@gmail.com>
2020-08-31 00:16:05 +0900

    add version command

---
+++ src/command/version.ts
@@ -0,0 +1,12 @@
+import { version } from '../../package.json';
+import { box } from '../printer/box';
+import * as chalk from 'chalk';
+async function command(): Promise<void> {
+  const updateAvailable = false;
+  if (updateAvailable) {
+    // todo - version checking
+  }
+  console.log(box(`fzr version ${chalk.greenBright(version)}`));
+}
+
+export default command;
