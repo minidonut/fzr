7fd00c980269ec8c43ab4f03af4df760a4fef931
minidonut <nycom13@gmail.com>
2020-09-02 02:21:57 +0900

    add configPath to context

--- src/context/env.ts
+++ src/context/env.ts
@@ -1,10 +1,19 @@
 import * as os from 'os';
 import * as path from 'path';
 
+interface Context {
+  basePath: string;
+  profile: string;
+  configPath?: string;
+}
+
 const context: Context = {
   basePath: process.env.FZR_PATH ?? path.join(os.homedir(), '.fzr'),
   profile: process.env.FZR_PROFILE ?? 'default',
+  configPath: undefined,
 };
+
+context.configPath = context.basePath;
 
 // freeze basepath
 if (process.argv[2] !== 'init') {
