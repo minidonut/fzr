3219f16c0b3a71e50fccdbea96f8b85d27c66a69
minidonut <nycom13@gmail.com>
2020-09-04 03:26:36 +0900

    open url or copy snippet

--- src/command/open/index.ts
+++ src/command/open/index.ts
@@ -1,11 +1,27 @@
 import { find } from '../../utils/find';
 import { getDatabase } from '../../database';
+import * as open from 'open';
+import * as chalk from 'chalk';
+import * as clipboardy from 'clipboardy';
 
 async function command(): Promise<void> {
   const [selected, database] = await Promise.all([find('open'), getDatabase()]);
   const key = selected.slice(132);
   const item = await database.get(key);
-  console.log('open!', key, item);
+
+  if (item.resource.startsWith('http')) {
+    // resource is url
+    const urls = item.resource.split(',');
+    for (const url of urls) {
+      await open(url.trim());
+      console.log(`open ${chalk.blueBright(url)}`);
+    }
+  } else {
+    // resource is snippet
+    console.log(item.resource);
+    await clipboardy.write(item.resource);
+    console.log(chalk.greenBright('copied'), '👍');
+  }
 }
 
 export default command;
