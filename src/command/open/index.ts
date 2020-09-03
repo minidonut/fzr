8c53fb46115ddfbfece1da41a04c6f61d9000e24
minidonut <nycom13@gmail.com>
2020-09-03 22:19:35 +0900

    throw NotInitializedError when config is loaded

--- src/command/open/index.ts
+++ src/command/open/index.ts
@@ -1,13 +1,7 @@
 import { config } from '../../context/config';
-import * as R from 'ramda';
-import { NotInitializedError } from '../../error';
 
 async function command(): Promise<void> {
-  if (R.isEmpty(config)) {
-    throw new NotInitializedError();
-  }
-
-  console.log('open!');
+  console.log('open!', config);
 }
 
 export default command;
--- src/context/config.ts
+++ src/context/config.ts
@@ -1,6 +1,12 @@
+import * as R from 'ramda';
+import { NotInitializedError } from '../error';
 import { env } from './env';
 import * as fs from 'fs-extra';
 
 const config = fs.readJSONSync(env.configPath, { throws: false }) ?? {};
+
+if (R.isEmpty(config)) {
+  throw new NotInitializedError();
+}
 
 export { config };
