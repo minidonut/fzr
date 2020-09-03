eb9bbd4079cbe4ed48749ca8f7e7e41a8b735ae6
minidonut <nycom13@gmail.com>
2020-09-04 00:16:50 +0900

    env export profilePath (computed)

--- src/context/env.ts
+++ src/context/env.ts
@@ -5,16 +5,19 @@ interface Env {
   basePath: string;
   profile: string;
   configPath: string;
+  profilePath: string;
 }
 
 const basePath = process.env.FZR_PATH ?? path.join(os.homedir(), '.fzr');
 const profile = process.env.FZR_PROFILE ?? 'default';
 const configPath = path.join(basePath, 'config.json');
+const profilePath = path.join(basePath, profile);
 
 const env: Env = {
   basePath,
   profile,
   configPath,
+  profilePath,
 };
 
 // freeze basepath
