f4c83b6031e2d09905bf819c84320d71acf5ddd4
minidonut <nycom13@gmail.com>
2020-09-04 03:57:11 +0900

    update open command to start fast

---
+++ src/command/open/open.ts
@@ -0,0 +1,26 @@
+import * as open from 'open';
+import { getDatabase } from '../../database';
+import * as chalk from 'chalk';
+import * as clipboardy from 'clipboardy';
+
+async function command(key: string): Promise<void> {
+  const database = await getDatabase();
+  const item = await database.get(key);
+
+  if (item.resource.startsWith('http')) {
+    // resource is url
+    const urls = item.resource.split(',');
+    for (const url of urls) {
+      await open(url.trim());
+      console.log(`open ${chalk.blueBright(url.trim())}`);
+    }
+  } else {
+    // resource is snippet
+    console.log(item.resource);
+    await clipboardy.write(item.resource);
+    console.log(chalk.greenBright('copied'), '👍');
+  }
+  process.exit(0);
+}
+
+export default command;
