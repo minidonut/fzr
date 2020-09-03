c38d947bdf43a0e9f3cee07ea6955aef9af545f6
minidonut <nycom13@gmail.com>
2020-09-04 02:25:04 +0900

    add more common types

--- src/model.d.ts
+++ src/model.d.ts
@@ -1,1 +1,21 @@
 export type DatabaseType = 'json' | 'sqlite' | 'dynamodb' | 'mysql';
+export type Profile = {
+  database: string;
+  data: Record<string, string | number>;
+};
+export type Config = Record<string, Profile>;
+export interface Item {
+  title: string;
+  resource: string;
+  body: string;
+  createdAt: number;
+  accessedAt: number;
+  accessedCount: number;
+}
+export interface Database {
+  create: (key: string, item: Item) => Promise<Item>;
+  read: (key: string) => Promise<Item>;
+  update: (key: string, item: Item) => Promise<Item>;
+  delete: (key: string) => Promise<void>;
+  length: () => Promise<number>;
+}
